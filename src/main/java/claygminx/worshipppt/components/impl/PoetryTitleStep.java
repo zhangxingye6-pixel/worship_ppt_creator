8b056387dd3e4a8f4e689f6caf0dfb3ca232a6f6
lazarusliu <afung730303@outlook.com>
2026-05-22 19:50:27 +0800

    2-1模式 第三首敬拜诗歌封面制作流程更正

--- src/main/java/claygminx/worshipppt/components/impl/PoetryTitleStep.java
+++ src/main/java/claygminx/worshipppt/components/impl/PoetryTitleStep.java
@@ -2,11 +2,14 @@ package claygminx.worshipppt.components.impl;
 
 import claygminx.worshipppt.common.Dict;
 import claygminx.worshipppt.common.config.SystemConfig;
+import claygminx.worshipppt.common.entity.PoetryEntity;
 import claygminx.worshipppt.exception.PPTLayoutException;
 import claygminx.worshipppt.util.TextUtil;
 import org.apache.poi.xslf.usermodel.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * 诗歌标题阶段
@@ -17,25 +20,43 @@ public class PoetryTitleStep extends AbstractWorshipStep {
 
     private final String slideName;
     private final String poetryName;
+    private final String albumName;
+    private final int mode;
+    private final List<PoetryEntity> fullPoetryList;
 
     // 字体常量
     private final double DEFAULT_POETRY_COVER_FONT_SIZE = 55.0;
     private final double DEFAULT_POETRY_TITLE_FONT_SIZE = 40.0;
 
     public PoetryTitleStep(XMLSlideShow ppt, String layout, String slideName, String poetryName) {
+        this(ppt, layout, slideName, poetryName, null, 0, null);
+    }
+
+    public PoetryTitleStep(XMLSlideShow ppt, String layout, String slideName, String poetryName,
+                           String albumName, int mode, List<PoetryEntity> fullPoetryList) {
         super(ppt, layout);
         this.slideName = slideName;
         this.poetryName = poetryName;
+        this.albumName = albumName;
+        this.mode = mode;
+        this.fullPoetryList = fullPoetryList;
     }
 
     @Override
     public void execute() throws PPTLayoutException {
+        String effectivePoetryName = poetryName;
+        if ("敬拜诗歌".equals(albumName) && mode == 1 && fullPoetryList != null && fullPoetryList.size() >= 3) {
+            if (fullPoetryList.size() > 1 && poetryName.equals(fullPoetryList.get(1).getName())) {
+                effectivePoetryName = fullPoetryList.get(2).getName();
+            }
+        }
+
         XMLSlideShow ppt = getPpt();
         XSLFSlideLayout layout = ppt.findLayout(getLayout());
         XSLFSlide slide = ppt.createSlide(layout);
 
         fillPlaceholder(slide, 0, " " + slideName);
-        fillPlaceholder(slide, 1, poetryName);
+        fillPlaceholder(slide, 1, effectivePoetryName);
 
         logger.info("诗歌标题 - 幻灯片制作完成");
     }
