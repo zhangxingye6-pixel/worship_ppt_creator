e2147cb72fb295abaa54ef59a3d7dd0480bfac9e
lazarusliu <afung730303@outlook.com>
2026-08-01 19:53:52 +0800

    更新版本提示下载链接

--- src/main/java/claygminx/worshipppt/components/impl/WorshipFormServiceImpl.java
+++ src/main/java/claygminx/worshipppt/components/impl/WorshipFormServiceImpl.java
@@ -1107,8 +1107,7 @@ public class WorshipFormServiceImpl implements WorshipFormService {
         // 版本信息
         String versionInfo = stringBuilder.append("发现新版本: ").append(githubReleaseEntity.tag_name()).append("\n")
                 .append("-").append("发布于: ").append(simpleDateFormat.format(githubReleaseEntity.created_at())).append("\n")
-                .append("-").append("下载地址: <a href='").append(githubReleaseEntity.html_url()).append("'>").append("点击前往下载win/macos_arm版本").append("</a>").append("\n")
-                .append("-").append("下载地址: <a href='https://codeberg.org/zhangxingye6-pixel/-/packages/generic/worship-ppt-creator/").append(githubReleaseEntity.tag_name()).append("'>").append("点击前往下载macos_x86_64版本").append("</a>").append("\n").toString();
+                .append("-").append("下载地址: <a href='").append(githubReleaseEntity.html_url()).append("'>").append("点击前往下载Windows/macOS Intel/macOS ARM版本").append("</a>").append("\n").toString();
         // 清空
         stringBuilder.setLength(0);
         // 新版本特性
